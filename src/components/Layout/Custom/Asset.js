c88a9200c80a53efe3b09b927e5871c1d2b23556
renegadefloofer <566976656b@gmail.com>
2022-02-26 21:39:20 -0500

    test

--- src/components/Layout/Custom/Asset.js
+++ src/components/Layout/Custom/Asset.js
@@ -1,6 +1,9 @@
+import Card from '../Card';
+
 const Asset = (props) => {
   return (
     <li>
+      <Card></Card>
       <div>{props.name}</div>
       <div>{props.price}</div>
       <div>{props.amount}</div>
